bd79ccc71ddefcda046ff79f24eae215ebc31ca2
Luis Enrique Vite Aquino <luis.vite0307@gmail.com>
2016-05-03 15:57:57 -0500

    ....

--- Metodos.h
+++ Metodos.h
@@ -1,6 +1,36 @@
 #ifndef METODOS_H_INCLUDED
 #define METODOS_H_INCLUDED
 
+typedef struct
+{
+    int clave;      //Clave de la muestra
+    char nombre[30];//Nombre de la muestra
+    char depto[];   //Departamento al que pertenece la muestra
+}Muestra;
+
+typedef struct
+{
+    int *arreglo;
+    int header;
+    int tail;
+    int max;
+}ColaLineal;
+
+typedef struct
+{
+    int *arreglo;
+    int header;
+    int tail;
+    int max;
+}ColaCircular;
+
+typedef struct
+{
+    int *arreglo;
+    int tope;
+    int max;
+}Pila;
+
 
 
 #endif // METODOS_H_INCLUDED
