f759b8cc242a3d12bb600c186997fb3b240b98e7
Gustavohbd <dagus_08@hotmail.com>
2016-05-05 13:25:10 -0500

    Update Principal.c

--- Principal.c
+++ Principal.c
@@ -45,12 +45,49 @@ int main()
 
         switch (opcion) {
             case 1: //Se ingresa una muestra a la cola lineal
+                    if(ValidaEspacioCL(cl))
+                        Insertar(cl);
+                    else
+                        printf("\nMuestras de entrada lleno...\n");
                 break;
             case 2: //Se ingresa una muestra a la cola circular
+                if(ValidaVacioCL(cl))
+                        printf("\nNo hay muestras para analizar\n");
+                    else
+                    {
+                        if(ValidaEspacioCC(cc))
+                        {
+                            //aux = Borrar(cl);
+                            //InsertarCC(cc,aux);
+                            //printf("\nAnalizada\n");
+                        }
+                        else
+                            printf("No hay espacio para analizar");
+                    }
                 break;
             case 3://Se ingresa una mustra a la pila
+                if(ValidaVacioCC(cc))
+                        printf("\nNo hay muestras para liberar(Verifique si se analizaron)\n");
+                    else
+                    {
+                        if(ValidaEspacioP(p))
+                        {
+                            aux = BorrarCC(cc);
+                            Push(p,aux);
+                            printf("\nLiberada\n");
+                        }
+                        else
+                            printf("\nNo hay espacio para liberar\n");
+                    }
                 break;
             case 4://Se saca una muestra de la pila
+                if(ValidaVacioP(p))
+                        printf("\nNo hay muestras para entregar\n");
+                    else
+                    {
+                        printf("\nMuestra entregada...\n");
+                        aux = Pop(p);
+                    }
                 break;
         }
     }while (opcion != 5);
